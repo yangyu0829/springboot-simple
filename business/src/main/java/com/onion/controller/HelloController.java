62bbb78c747e48f5884a7bb5af9b31cd968031ad
yu <773024110@qq.com>
2020-06-12 13:47:36 +0800

    增加 war包支持

--- business/src/main/java/com/onion/controller/HelloController.java
+++ business/src/main/java/com/onion/controller/HelloController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * @作者:yu
